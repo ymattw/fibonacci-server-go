96c5f2dfa11720c4612975cc5689f9b40c97dfb7
Matthew Wang <mattwyl@gmail.com>
2016-05-31 21:24:06 +0800

    Support taking argument from cmd line

--- cmd/fibonacci/fibonacci.go
+++ cmd/fibonacci/fibonacci.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path"
+	"strconv"
+
 	"github.com/ymattw/fibonacci-server-go/fibonacci"
 )
 
 func main() {
-	fmt.Println(fibonacci.Sequence(10))
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <n>\n", path.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	var n, err = strconv.Atoi(os.Args[1])
+	if err != nil || n < 0 {
+		fmt.Fprintf(os.Stderr, "Input must be a non-negative integer\n")
+		os.Exit(2)
+	}
+
+	fmt.Println(fibonacci.Sequence(n))
 }
