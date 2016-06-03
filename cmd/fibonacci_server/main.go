28ee7e0c88e78fc671e470482d4e6bc3f8e04c73
Matthew Wang <mattwyl@gmail.com>
2016-06-03 11:31:19 +0800

    Implement '/' route with httprouter

--- cmd/fibonacci_server/main.go
+++ cmd/fibonacci_server/main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Welcome to Fibonacci Server!\n\n"+
+		"Currently only one API is supported:\n\n"+
+		"GET /v1/fib/:number\n")
+}
+
+func fibonacci(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "[*] %s\n", r.URL.Path)
 }
 
 func main() {
 	var port = 9090
+	var router = httprouter.New()
 
-	http.HandleFunc("/", handler)
+	router.GET("/", index)
+	router.GET("/:version/fib/:number", fibonacci)
+
 	fmt.Printf("Will serving on port %d\n", port)
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
 }
