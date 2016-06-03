28a47b9e1908773751332e780c444d4d652a3e6b
Matthew Wang <mattwyl@gmail.com>
2016-06-03 12:54:58 +0800

    Implement the /:version/fib/:number API

--- cmd/fibonacci_server/main.go
+++ cmd/fibonacci_server/main.go
@@ -5,9 +5,13 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
+	. "github.com/ymattw/fibonacci-server-go/fibonacci"
 )
+
+const MaxAcceptableNumber = 10000
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Welcome to Fibonacci Server!\n\n"+
@@ -15,8 +19,34 @@ func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		"GET /v1/fib/:number\n")
 }
 
-func fibonacci(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "[*] %s\n", r.URL.Path)
+func fibonacci(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	var version = params.ByName("version")
+	var number = params.ByName("number")
+
+	log.Println("[*]", version, number)
+
+	if version != "v1" {
+		http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
+		return
+	}
+
+	var n, err = strconv.Atoi(number)
+	if err != nil || n < 0 {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	} else if n > MaxAcceptableNumber {
+		http.Error(w, http.StatusText(http.StatusRequestEntityTooLarge), http.StatusRequestEntityTooLarge)
+		return
+	}
+
+	var sequence = Sequence(n)
+	var strs = make([]string, n)
+	for i, v := range sequence {
+		strs[i] = v.String()
+	}
+
+	var out = strings.Join(strs, ", ")
+	fmt.Fprintf(w, "[%s]\n", out)
 }
 
 func main() {
@@ -26,6 +56,6 @@ func main() {
 	router.GET("/", index)
 	router.GET("/:version/fib/:number", fibonacci)
 
-	fmt.Printf("Will serving on port %d\n", port)
+	log.Println("Will serving on port", port)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
 }
